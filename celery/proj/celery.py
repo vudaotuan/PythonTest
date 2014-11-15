e379b3aa92c9b3a750d3fb66fe5004196d4afba1
vudt <vu.daotuan@gmail.com>
2014-11-15 10:13:51 +0700

    add inclue

--- celery/proj/celery.py
+++ celery/proj/celery.py
@@ -3,7 +3,8 @@ from __future__ import absolute_import
 from celery import Celery
 
 app = Celery('proj',
-             broker='amqp://guest:guest@localhost:5672//')
+             broker='amqp://guest:guest@localhost:5672//', ,
+             include=['proj.tasks'])
 
 # Optional configuration, see the application user guide.
 app.conf.update(
