b1bcd973224906701b424beb0f4ea37f78e8187d
vudaotuan <vu.daotuan@gmail.com>
2014-11-24 15:58:21 +0700

    a

--- celery/batch/tasks.py
+++ celery/batch/tasks.py
@@ -7,8 +7,8 @@ app = Celery('tasks', broker='amqp://guest:guest@localhost:5672//')
 @app.task(base=Batches, flush_every=5, flush_interval=10)
 def add(items):
 	total = 0
-    for x in items:
-    	total = total + x
+	for x in items:
+		total = total + x
 
     print('>>> Total: {0}'.format(total))
     return total
