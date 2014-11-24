517b3cb2fa38b5e1a7fabdf04e3b9883957ea220
vudaotuan <vu.daotuan@gmail.com>
2014-11-24 15:56:09 +0700

    a

--- celery/batch/tasks.py
+++ celery/batch/tasks.py
@@ -1,9 +1,14 @@
 __author__ = 'Administrator'
-
+from celery.contrib.batches import Batches
 from celery import Celery
 
 app = Celery('tasks', broker='amqp://guest:guest@localhost:5672//')
 
-@app.task
-def add(x, y):
-    return x + y
+@app.task(base=Batches, flush_every=5, flush_interval=10)
+def add(items):
+	total = 0
+    for x in items:
+    	total = total + x
+
+    print('>>> Total: {0}'.format(total))
+    return total
